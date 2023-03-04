93a447417db8fe409cd9c0d246145647ee4929e6
Yoshihiro Tanaka <contact@cordea.jp>
2023-03-04 12:49:46 +0900

    add a blueprint function library

---
+++ Source/kast/kast.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "kast.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, kast, "kast" );
