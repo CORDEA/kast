dc4eb40a8d80de945864692da1087af19536a08c
Yoshihiro Tanaka <contact@cordea.jp>
2023-03-10 12:22:45 +0900

    define events

--- Source/kast/MainUserWidget.cpp
+++ Source/kast/MainUserWidget.cpp
@@ -3,3 +3,51 @@
 
 #include "MainUserWidget.h"
 
+#include <string>
+
+#include "HttpModule.h"
+#include "ServerSettings.h"
+
+void UMainUserWidget::OnSubmitSucceeded_Implementation()
+{
+}
+
+void UMainUserWidget::OnSubmitFailed_Implementation()
+{
+}
+
+void UMainUserWidget::OnSubmitted(
+	FString Length,
+	FString ShoulderLength,
+	FString Width,
+	FString SleeveLength,
+	FString Yukitake
+)
+{
+	const auto Settings = GetDefault<UServerSettings>();
+	auto HTTPModule = &FHttpModule::Get();
+	auto Request = HTTPModule->CreateRequest();
+	Request->SetVerb("POST");
+	Request->SetHeader("Content-Type", "application/json");
+
+	TSharedRef<FJsonObject> JSON = MakeShareable(new FJsonObject());
+	JSON->SetNumberField("length", std::stod(*Length));
+	JSON->SetNumberField("shoulderLength", std::stod(*ShoulderLength));
+	JSON->SetNumberField("width", std::stod(*Width));
+	JSON->SetNumberField("sleeveLength", std::stod(*SleeveLength));
+	JSON->SetNumberField("yukitake", std::stod(*Yukitake));
+	FString JSONString;
+	const auto Writer = TJsonWriterFactory<>::Create(&JSONString);
+	FJsonSerializer::Serialize(JSON, Writer);
+
+	Request->SetContentAsString(JSONString);
+	Request->SetURL("https://" + Settings->Host + Settings->PostTopsPath);
+	Request->OnProcessRequestComplete().BindLambda([&](
+		FHttpRequestPtr,
+		FHttpResponsePtr,
+		bool)
+		{
+		}
+	);
+	Request->ProcessRequest();
+}
--- Source/kast/MainUserWidget.h
+++ Source/kast/MainUserWidget.h
@@ -13,5 +13,20 @@ UCLASS()
 class KAST_API UMainUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintNativeEvent)
+	void OnSubmitSucceeded();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void OnSubmitFailed();
+
+	UFUNCTION(BlueprintCallable)
+	void OnSubmitted(
+		FString Length,
+		FString ShoulderLength,
+		FString Width,
+		FString SleeveLength,
+		FString Yukitake
+	);
 };
